93c2edd286afbd048e68bee8448ddcf65459cb84
Priyachi2003 <96510349+Priyachi2003@users.noreply.github.com>
2022-03-17 17:23:46 +0000

    reverse string

--- p6original.c
+++ p6original.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
-void input_string(char a)
+#include<string.h>
+void input_string(char *a)
 {
+  printf("enter the word\n");
+  scanf("%s",a);
   
+}
+char str_reverse(char *a)
+{
+  for(int i=0;a[i]!='\0';i++)
+    {
+       printf("%d",a[i]);
+    }
+    
+}
+void output(char *a,char *reversea)
+{
+  for(int i=4;a[i]!=0;i--)
+    {
+      printf("%d",reversea[i]-1);
+    }
+}
+int main()
+{
+  char str[20],a;
+  input_string(str);
+  str_reverse(str);
+  output(&a,str);
 }
--- p7original.c
+++ p7original.c
@@ -1,6 +1,32 @@
 #include<stdio.h>
-struct _triangle
+#include<string.h>
+char input_string(char*a)
 {
-float base,altitude,area;
-}; <brtypedef _triangle Triangle
-Triangle input_triangle()
+  printf("enter the word\n");
+  scanf("%s",a);
+}
+char str_reverse(char *str)
+{
+  char temp;
+  int i=0,j=0;
+  j=strlen(str)-1;
+  while(i<j)
+    {
+      temp=str[i];
+      str[i]=str[j];
+      str[j]=temp;
+      i++;
+      j--;
+    }
+}
+void output(char *reversea)
+{
+  printf("the reverse string is %s",reversea);
+}
+int main()
+{
+  char str[20];
+  input_string(str);
+  str_reverse(str);
+  output(str);
+}
