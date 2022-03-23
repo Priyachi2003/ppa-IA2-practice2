a67fb7381fb7556a598a9dcc2baabcb543800971
Priyachi2003 <96510349+Priyachi2003@users.noreply.github.com>
2022-03-23 05:02:05 +0000

    p8

--- p8original.c
+++ p8original.c
@@ -1,30 +1,22 @@
-#include<stdio.h>
 struct _triangle
 {
-float b,h,area;
-}; 
-  typedef struct _triangle Triangle;
+float base,altitude,area;
+};
+typedef struct _triangle Triangle;
+int input_n()
+{
+  Triangle n;
+  printf("enter the values\n");
+  scanf("%f",&n);
+  return n;
+}
 Triangle input_triangle()
 {
   Triangle a;
-  printf("enter the values\n");
-  scanf("%f %f",&a.b,&a.h);
-  return a;
+  printf("enter the values of base and altitude\n");
+  scanf("%f %f",a.base,a.altitude);
 }
-void find_area(Triangle*t)
+void input_n_triangles(int n, triangle t[n])
 {
-  t->area=0.5 * t->b * t->h;
-}
-void output(Triangle t)
-{
-  printf("the area of traingle with base %f &  altitude %f is %f",t.b,t.h,t.area);
-}
-int main()
-{
-  Triangle a;
-  a=input_triangle();
-  find_area(&a);
-  output(a);
-  return 0;
   
 }
